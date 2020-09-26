5d594252f71e24b90bbd9ba90a2829fd951eb133
eshoiza <engrjayt200@gmail.com>
2020-09-27 00:41:58 +0100

    Create a route for post

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv/config");
 
 //Midlewares
-app.use('/post', ()=>{
-    console.log("We are using a midleware")
-})
+app.use("/post", ()=>{
+    console.log("We are using a midleware");
+});
 
 //ROUTES
 app.get('/',(req, res)=>{
@@ -15,10 +16,16 @@ app.get('/',(req, res)=>{
 app.get("/post", (req, res) => {
   res.send("This is my new post");
 });
+
+// Connect to database
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to DB")
+);
+
 //App listening port
-
 const port = 3000;
-
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`);
 })
