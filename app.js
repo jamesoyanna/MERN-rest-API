ae6adf02479378a9bad2d6c535418149e9f86595
eshoiza <engrjayt200@gmail.com>
2020-09-26 23:34:17 +0100

    Implement a middleware

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const app = express();
 
+//Midlewares
+app.use('/post', ()=>{
+    console.log("We are using a midleware")
+})
+
 //ROUTES
 app.get('/',(req, res)=>{
     res.send("Hello fellow Nigerians")
 })
+
+app.get("/post", (req, res) => {
+  res.send("This is my new post");
+});
 //App listening port
 
 const port = 3000;
